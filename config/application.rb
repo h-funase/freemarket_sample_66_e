20e6b9ba77d04e1eb551c0dc4eaf41c4d0bcbc3b
h-funase <h.funse@gmail.com>
2020-01-07 21:21:23 +0900

    application.rb_add
    rails aborted!
    LoadError: cannot load such file -- uglifier
    というエラーが出て進まないため、こちらのコードを追加

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,11 @@
+config.assets.initialize_on_precompile = false
+#↑を追加
+
 require_relative 'boot'
 
 require 'rails/all'
+
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
