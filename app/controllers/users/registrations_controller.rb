6195187cb07c8683e5a6553722a34b8a8cce0710
h-funase <h.funse@gmail.com>
2020-01-13 13:16:11 +0900

    user_registrations_controller_create
    ユーザー登録用のコントローラーを作成しました。
    step1(名前、生年月日などの登録画面)、step2（step1の情報をsessionで引き継いだ電話番号入力画面を管理します）

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,60 +3,52 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
-
+  def step1
+    @user = User.new
+      session[:provider] = session[:provider]
+      session[:uid] = session[:uid]
+    # @user = User.create(nickname:session[:nickname], email: session[:email], password: session[:password],  first_name_kana: session[:first_name_kana],last_name_kana: session[:last_name_kana], first_name: session[:first_name], last_name: session[:last_name], birthday: session[:birthday])
+  end
   # POST /resource
-  # def create
-  #   super
-  # end
+  def step2
+    session[:nickname] = params[:user][:nickname]
+    session[:email] = params[:user][:email]
+    session[:password] = params[:user][:password]
+    session[:first_name_kana] = params[:user][:first_name_kana]
+    session[:last_name_kana] = params[:user][:last_name_kana]
+    session[:first_name] = params[:user][:first_name]
+    session[:last_name] = params[:user][:last_name]
+    session[:birthday] = birthday_join
+    session[:provider] = session[:provider]
+    session[:uid] = session[:uid]
+    @user = User.new(nickname:session[:nickname], email: session[:email], password: session[:password],  first_name_kana: session[:first_name_kana],last_name_kana: session[:last_name_kana], first_name: session[:first_name], last_name: session[:last_name], birthday: session[:birthday],phone: params[:user][:phone])
+  end
 
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  def create
+    @user = User.create(nickname:session[:nickname], email: session[:email], password: session[:password],  first_name_kana: session[:first_name_kana],last_name_kana: session[:last_name_kana], first_name: session[:first_name], last_name: session[:last_name], birthday: session[:birthday],phone: params[:user][:phone])
+    if @user.save
+      redirect_to controller: '/addresses', action: 'step3'
+      sign_in(@user)
+      # bypass_sign_in(@user)
+    else
+      render "step1"
+    end
+  end
 
-  # PUT /resource
-  # def update
-  #   super
-  # end
-
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
-
-  # protected
-
+  protected
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def configure_sign_up_params
+    # ↓基本はこれを使う
+    # devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation, :remember_me])
+  end
+  
+  def birthday_join
+    year = params[:user]["birthday(1i)"]
+    month = params[:user]["birthday(2i)"]
+    day = params[:user]["birthday(3i)"]
+    birthday = year.to_s + "-" + month.to_s + "-" + day.to_s
+    return birthday
+  end
 end
