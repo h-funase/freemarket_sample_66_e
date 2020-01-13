44c4c20765b7c6ed3e66182f627ca3ce7021701b
h-funase <h.funse@gmail.com>
2020-01-13 13:41:12 +0900

    application_controller_permit_phone
    電話番号のparamsをpermitで許可するよう編集しました

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
   before_action :basic_auth, if: :production?
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-  
 
   private
   def production?
@@ -18,6 +15,6 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday,:phone])
   end
 end
