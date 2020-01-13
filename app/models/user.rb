b39206c955fa5e562495b68456e3cd4aacf124d2
h-funase <h.funse@gmail.com>
2020-01-13 17:01:51 +0900

    user_model_year_month_day_format
    生年月日の入力フォームのデータが正常に保存されるように正規表現でフォーマットを指定しました

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday, presence: true
+         year_month_day = /\A\d{4}-\d{2}-\d{2}\z/
+  validates :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana,  presence: true
+  validates :birthday,  presence: true, format: { with: year_month_day }
+  has_one :address
 end
