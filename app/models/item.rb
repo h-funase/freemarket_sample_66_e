e3ebcc6ffdd98b114e3188583a1ee49a0beb2e03
ShunMatsumoto <mr.bigmr.little@gmail.com>
2020-01-15 20:26:30 +0900

    index_link_image
    imageテーブル を作成し、外部キーとしてitem_idを持たせることで出品されている商品とその画像を紐付け、トップページで新着アイテムを表示する時に画像も表示させることができる実装を施した。

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,12 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category
   belongs_to_active_hash :condition
   belongs_to_active_hash :size
   belongs_to_active_hash :delivery_charge
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :delivery_days
   belongs_to_active_hash :delivery_way
+
+  has_many :images, dependent: :destroy
 
 end
