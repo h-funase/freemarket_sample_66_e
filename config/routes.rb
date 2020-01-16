8cd05b47267caeb389272b68b166a0d533bfcd6f
h-funase <h.funse@gmail.com>
2020-01-16 21:54:32 +0900

    ルーティング編集（ユーザー登録用）
    deviseに紐づいたuserのルーティングを作成しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,30 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+  devise_scope :user do
+    get    'users/signup/registration',      to: 'users/registrations#step1'
+    get    'users/signup/sms_confirmation',  to: 'users/registrations#step2'
+  end
+  
+  get '/addresses/new', to: 'addresses#step3'
+  get '/cards/new', to: 'cards#step4'
+  get '/signup/done', to: 'signup#done'
+
+  resources :signup do
+    collection do
+      get 'index'
+      get 'done' 
+    end
+  end
+
   root 'items#index'
+  resources :items, only:[:index, :show,]
+  resources :cards,only:[:create]
+  resources :addresses,only:[:create,:update]
+  resources :logout, only: [:index]
+
 
   resources :items do
     collection do
@@ -20,4 +45,5 @@ Rails.application.routes.draw do
   end
 
   resources :mypages, only: [:index, :show,]
+
 end
