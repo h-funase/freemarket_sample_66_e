05a39eee1c9299d30bb7125077d47beda90913de
h-funase <h.funse@gmail.com>
2020-01-17 12:50:06 +0900

    schema.rbとfakerを再インストール

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200115041108) do
-
-  create_table "images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "item_id",    null: false
-    t.string   "image",      null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["item_id"], name: "index_images_on_item_id", using: :btree
-  end
+ActiveRecord::Schema.define(version: 20200117022212) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id",         null: false
@@ -45,12 +37,32 @@ ActiveRecord::Schema.define(version: 20200115041108) do
     t.index ["user_id"], name: "index_cards_on_user_id", using: :btree
   end
 
+  create_table "images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "item_id",    null: false
+    t.string   "image",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_images_on_item_id", using: :btree
+  end
+
+  create_table "installs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_installs_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_installs_on_reset_password_token", unique: true, using: :btree
+  end
+
   create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id",                       null: false
     t.string   "name",                          null: false
     t.text     "description",     limit: 65535
     t.integer  "category_id",                   null: false
-    t.string   "size",                          null: false
+    t.integer  "size",                          null: false
     t.integer  "condition",                     null: false
     t.string   "delivery_charge",               null: false
     t.string   "delivery_way",                  null: false
@@ -63,8 +75,6 @@ ActiveRecord::Schema.define(version: 20200115041108) do
     t.datetime "updated_at",                    null: false
     t.index ["user_id"], name: "index_items_on_user_id", using: :btree
   end
-
-  add_foreign_key "images", "items"
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "nickname",                            null: false
@@ -87,5 +97,5 @@ ActiveRecord::Schema.define(version: 20200115041108) do
   end
 
   add_foreign_key "cards", "users"
-
+  add_foreign_key "images", "items"
 end
