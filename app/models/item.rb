2ce08d811da989b17b092c69bfcd608fa709f9e8
ShunMatsumoto <mr.bigmr.little@gmail.com>
2020-01-17 18:48:23 +0900

    prefecture_active_hash

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   # belongs_to_active_hash :condition
   # belongs_to_active_hash :size
   # belongs_to_active_hash :delivery_charge
-  # belongs_to_active_hash :prefecture
+  belongs_to_active_hash :prefecture
   # belongs_to_active_hash :delivery_days
   # belongs_to_active_hash :delivery_way
 
