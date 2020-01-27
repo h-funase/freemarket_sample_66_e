9112c750086393400ab8c38a3f834f2bfed50bf3
h-funase <h.funse@gmail.com>
2020-01-27 11:22:58 +0900

    SNS認証調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
 
-  devise_for :installs
 
   devise_for :users, controllers: {
     sessions: 'users/sessions',
-    registrations: 'users/registrations',
-    omniauth_callbacks: 'users/omniauth_callbacks'
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
   }
   devise_scope :user do
     get    'users/signup/registration',      to: 'users/registrations#step1'
