4cb2879af15b4bddcbbdd438e038560970f2557a
chanken2314 <57705822+chanken2314@users.noreply.github.com>
2020-01-28 13:36:24 +0900

    test

---
+++ app/assets/javascripts/edit.js
@@ -0,0 +1,79 @@
+// $(function(){
+//   $fileField = $('#file')
+ 
+//   // 選択された画像を取得し表示
+//   $($fileField).on('change', $fileField, function(e) {
+//     file = e.target.files[0]
+//     reader = new FileReader(),
+//     $preview = $("#img_field");
+
+ 
+//     reader.onload = (function(file) {
+//       return function(e) {
+//         $preview.empty();
+//         $preview.append($('<img>').attr({
+//           src: e.target.result,
+//           width: "30%",
+//           class: "preview",
+//           title: file.name
+//         }));
+//       };
+//     })(file);
+//     reader.readAsDataURL(file);
+//   });
+// });
+
+$(function(){
+  //DataTransferオブジェクトで、データを格納する箱を作る
+  var dataBox = new DataTransfer();
+  //querySelectorでfile_fieldを取得
+  var file_field = document.querySelector('input[type=file]')
+  //fileが選択された時に発火するイベント
+  $('#file').change(function(){
+    //選択したfileのオブジェクトをpropで取得
+    var files = $('input[type="file"]').prop('files')[0];
+    $.each(this.files, function(i, file){
+      //FileReaderのreadAsDataURLで指定したFileオブジェクトを読み込む
+      var fileReader = new FileReader();
+
+      //DataTransferオブジェクトに対して、fileを追加
+      dataBox.items.add(file)
+      //DataTransferオブジェクトに入ったfile一覧をfile_fieldの中に代入
+      file_field.files = dataBox.files
+
+      var num = $('.item-image').length + 1 + i
+      fileReader.readAsDataURL(file);
+    //画像が10枚になったら超えたらドロップボックスを削除する
+      if (num == 10){
+        $('#image-box__container').css('display', 'none')   
+      }
+      //読み込みが完了すると、srcにfileのURLを格納
+      fileReader.onloadend = function() {
+        var src = fileReader.result
+        var html= `<div class='item-image' data-image="${file.image_url}">
+                    <div class=' item-image__content'>
+                      <div class='item-image__content--icon'>
+                        <img src=${src} width="114" height="80" >
+                      </div>
+                    </div>
+                    <div class='item-image__operetion'>
+                      <div class='item-image__operetion--delete'>削除</div>
+                    </div>
+                  </div>`
+
+        $('#image-box__container').before(html);
+      };
+      //image-box__containerのクラスを変更し、CSSでドロップボックスの大きさを変えてやる。
+      $('#image-box__container').attr('class', `item-num-${num}`)
+    });
+  });
+  //削除ボタンをクリックすると発火するイベント
+  $(document).on("click", '.item-image__operetion--delete', function(){
+    //プレビュー要素を取得
+    var target_image = $(this).parent().parent()
+    //プレビューを削除
+    target_image.remove();
+    //inputタグに入ったファイルを削除
+    file_field.val("")
+  })
+});
