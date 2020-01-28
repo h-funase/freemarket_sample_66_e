e7dcc1abe89b7c67a61e65a463b66eb594f226ae
h-funase <h.funse@gmail.com>
2020-01-28 13:34:51 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -80,24 +80,22 @@ ActiveRecord::Schema.define(version: 20200122030254) do
 
   create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
-
-
-    t.string   "name",                             null: false
-    t.text     "description",        limit: 65535, null: false
-    t.string   "category_id",                      null: false
-    t.integer  "size_id"
-    t.string   "condition_id",                     null: false
-    t.string   "delivery_charge_id",               null: false
-    t.string   "prefecture_id",                    null: false
-    t.string   "delivery_days_id",                 null: false
-    t.string   "delivery_way_id",                  null: false
-    t.integer  "price",                            null: false
+    t.string   "name",               null: false
+    t.string   "category_id",        null: false
+    t.string   "size_id"
+    t.string   "description"
+    t.string   "condition_id",       null: false
+    t.string   "delivery_charge_id", null: false
+    t.string   "prefecture_id",      null: false
+    t.string   "delivery_days_id",   null: false
+    t.string   "delivery_way_id",    null: false
+    t.integer  "price",              null: false
     t.integer  "buyer_id"
     t.integer  "seller_id"
     t.string   "brand_id"
-    t.integer  "status"
-    t.datetime "created_at",                       null: false
-    t.datetime "updated_at",                       null: false
+    t.string   "status"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.index ["user_id"], name: "index_items_on_user_id", using: :btree
   end
 
